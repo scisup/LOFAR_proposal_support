b667b66e8243b9acd0d72eac0e682675c733735b
sarrvesh <sarrvesh.ss@gmail.com>
2020-02-15 11:48:11 +0100

    initial commit

---
+++ frontend.py
@@ -0,0 +1,81 @@
+"""Main file"""
+
+__author__ = "Sarrvesh S. Sridhar"
+__email__ = "sarrvesh@astron.nl"
+
+import dash
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, State, Input
+from gui import layout
+import flask
+import DegSupIndicator_v0 as d
+
+# Initialize the dash app
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUMEN], \
+                server=server)
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+#######################################
+# Setup the layout of the web interface
+#######################################
+app.layout = layout
+app.title = 'LOFAR Project Support Estimator'
+
+#######################################
+# What should the Calculate button do?
+#######################################
+def str2bool(var):
+    if var == 'True':
+        return True
+    else:
+        return False
+
+@app.callback(
+    Output('deg_support', 'value'),
+    [Input('calculate','n_clicks')],
+    [State('prop_type_drop', 'value'),
+     State('obs_type_drop', 'value'),
+     State('nObsHours', 'value'),
+     State('obs_strategy_drop', 'value'),
+     State('nObsRuns', 'value'),
+     State('nSAP', 'value'),
+     State('nPipe', 'value'),
+     State('para_obs_drop', 'value'),
+     State('array_req_drop', 'value'),
+     State('rt_drop', 'value'),
+     State('change_target_drop', 'value'),
+     State('dynspec_drop', 'value')
+    ]
+)
+def on_calculate_click(n, prop_type, obs_type, n_hours_total, \
+                       obs_strategy, n_runs, n_sap, n_pipe, para_obs, \
+                       array_req, rt, change_tar, dynspec):
+    if n is None:
+        # The page has just loaded
+        return ''
+    else:
+        # User has clicked on the button
+        output_str = d.main(
+            Projtype = prop_type,
+            Tobsreq  = n_hours_total,
+            Obsmode = obs_type,
+            Obsstrategytype = obs_strategy,
+            Parallelobs = str2bool(para_obs),
+            Nruns = n_runs,
+            Nsap = n_sap,
+            Arrconstr = str2bool(array_req),
+            Schedconstr = False,
+            Resptel = str2bool(rt),
+            Targlistupd = str2bool(change_tar),
+            Npipxobs = n_pipe,
+            Dynsp = str2bool(dynspec),
+            Debug=False
+        )        
+        return output_str
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8052)
+    #app.run_server(debug=False, host='0.0.0.0', port=8052, \
+    #              dev_tools_ui=False, dev_tools_props_check=False)
---
+++ gui.py
@@ -0,0 +1,213 @@
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+
+header = html.Div([
+            html.H1('LOFAR Project Support Estimator'),
+         ])
+
+labelWidth = 6
+inpWidth = 4
+dropWidth = 4
+
+# PROPOSAL TYPE
+prop_type = dbc.FormGroup([
+                dbc.Label('Proposal type', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                            {'label':'Long term', 'value':'LT'},
+                            {'label':'Single cycle', 'value':'LC'}
+                        ], value='LC', 
+                        searchable=False, clearable=False, 
+                        id='prop_type_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# Observation type
+obs_type = dbc.FormGroup([
+                dbc.Label('Observation type', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                            {'label':'IF (interferometric)', 'value':'IF'},
+                            {'label':'BF (beamformed)', 'value':'BF'},
+                            {'label':'IF+BF', 'value':'IF+BF'},
+                            {'label':'TBB', 'value':'TBB'}
+                        ], value='IF', 
+                        searchable=False, clearable=False, 
+                        id='obs_type_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# Total observing hours
+obs_hours_total = dbc.FormGroup([
+                dbc.Label('Total observing hours in the proposal', width=labelWidth),
+                dbc.Col(
+                    dbc.Input(type='number',
+                              id='nObsHours',
+                              value=1,
+                              min=0
+                    ), width=inpWidth
+                )
+            ], row=True)
+
+# Observing strategy
+obs_strategy = dbc.FormGroup([
+                dbc.Label('Observation strategy', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'bookended', 'value':'bookended'},
+                          {'label':'interleaved', 'value':'interleaved'},
+                          {'label':'no-LST-constraint', 'value':'noLST-constraint'},
+                          {'label':'no-Calibrator-needed', 'value':'noCALIBRATOR-needed'}
+                        ], value='noLST-constraint', 
+                        searchable=False, clearable=False, 
+                        id='obs_strategy_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# No. of individual runs
+runs = dbc.FormGroup([
+                dbc.Label('Number of observing runs', width=labelWidth),
+                dbc.Col(
+                    dbc.Input(type='number',
+                              id='nObsRuns',
+                              value=1,
+                              min=1
+                    ), width=inpWidth
+                )
+            ], row=True)
+
+# No. of subbands per observation
+nsap = dbc.FormGroup([
+                dbc.Label('Number of SAPs per observation (0-488)', width=labelWidth),
+                dbc.Col(
+                    dbc.Input(type='number',
+                              id='nSAP',
+                              value=1,
+                              min=1,
+                              max=488
+                    ), width=inpWidth
+                )
+            ], row=True)
+
+# No. of subbands per observation
+npipe = dbc.FormGroup([
+                dbc.Label('Number of pipelines per observation', width=labelWidth),
+                dbc.Col(
+                    dbc.Input(type='number',
+                              id='nPipe',
+                              value=1,
+                              min=0
+                    ), width=inpWidth
+                )
+            ], row=True)
+
+# Observing strategy
+para_obs = dbc.FormGroup([
+                dbc.Label('Are parallel observations needed?', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'False', 'value':'False'},
+                          {'label':'True', 'value':'True'}
+                        ], value='False', 
+                        searchable=False, clearable=False, 
+                        id='para_obs_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# Observing strategy
+array_req = dbc.FormGroup([
+                dbc.Label('Do you need special array configuration?', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'False', 'value':'False'},
+                          {'label':'True', 'value':'True'}
+                        ], value='False', 
+                        searchable=False, clearable=False, 
+                        id='array_req_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# Observing strategy
+rt = dbc.FormGroup([
+                dbc.Label('Do you want to use responsive telescope?', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'False', 'value':'False'},
+                          {'label':'True', 'value':'True'}
+                        ], value='False', 
+                        searchable=False, clearable=False, 
+                        id='rt_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# 
+change_target = dbc.FormGroup([
+                dbc.Label('Will you change the target list during the cycle(s)?', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'False', 'value':'False'},
+                          {'label':'True', 'value':'True'}
+                        ], value='False', 
+                        searchable=False, clearable=False, 
+                        id='change_target_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+
+# Dynamic spectrum pipeline
+dynspec = dbc.FormGroup([
+                dbc.Label('Do you want to use the DynSpec pipeline?', width=labelWidth),
+                dbc.Col(
+                    dcc.Dropdown(
+                        options=[
+                          {'label':'False', 'value':'False'},
+                          {'label':'True', 'value':'True'}
+                        ], value='False', 
+                        searchable=False, clearable=False, 
+                        id='dynspec_drop'
+                    ), width=dropWidth
+                )
+            ], row=True)
+# Button
+buttons = dbc.FormGroup([
+                dbc.Label('', width=labelWidth),
+                dbc.Col(dbc.Button('Calculate', id='calculate', color='dark'))
+            ], row=True)
+
+form = dbc.Form([prop_type, obs_type, obs_hours_total, \
+                 obs_strategy, runs, nsap, npipe, \
+                 para_obs, array_req, rt, change_target, \
+                 dynspec, buttons])
+
+# Label to show output
+output = dbc.FormGroup([
+                dbc.Label('Required degree of support', width=labelWidth),
+                dbc.Col(
+                    dbc.Input(type='text',
+                              id='deg_support',
+                              value='',
+                              disabled=True
+                    ), width=inpWidth
+                )
+            ], row=True)
+
+layout = html.Div([
+            dbc.Row(dbc.Col(header)),
+            html.Hr(),
+            dbc.Row(dbc.Col(form)),
+            dbc.Row(dbc.Col(output))
+         ], style={'width':'35%', 'padding':'20px'})
